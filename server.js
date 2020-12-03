0e81a36c13810294a2ce0bff41ddd9948bf2d26a
April Darger <april.m.darger@gmail.com>
2020-12-02 17:13:02 -0700

    Got first middleware logger working.

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const helmet = require('helmet');
+//const postRouter = require('./posts/postRouter');
+const { logger } = require('./middleware/users-middleware');
 const server = express();
+
+server.use(logger);
+server.use(morgan('dev'));
+server.use(helmet());
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
-//custom middleware
-
-function logger(req, res, next) {}
 
 module.exports = server;
